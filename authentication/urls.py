5c4dbc4a407f105c1f1afebb7316d5fc68cddada
joy-amorin <amorinkaren2021@gmail.com>
2025-08-27 19:12:33 -0300

    Implement JWT authentication: login, logout, refresh, and token validation

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import LoginView
+from .views import LoginView, LogoutView
+from rest_framework_simplejwt.views import TokenRefreshView
+
 
 urlpatterns = [
-    path("login/", LoginView.as_view(), name='login')
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh")
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -4,7 +4,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import LoginSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
-from users.models import User
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.exceptions import TokenError
+
 
 class LoginView(APIView):
 
@@ -18,3 +20,19 @@ class LoginView(APIView):
                 "refresh": str(refresh)
             })
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"] #obtein the refresh token from the body
+            token = RefreshToken(refresh_token)
+            token.blacklist() #mark as invalid
+            return Response({"detail": "Logoutexitoso"}, status=status.HTTP_205_RESET_CONTENT)
+        
+        except KeyError:
+            return Response({"error": "Refresh token requerido"})
+        except TokenError:
+            return Response({"error": "Token inválido o expirado"})
+
