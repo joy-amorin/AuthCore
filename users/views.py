a1db243f2d1762ff9b7d2d172a3736414c83a9d4
joy-amorin <amorinkaren2021@gmail.com>
2025-12-04 19:49:57 -0300

    feat(user): add authentication user endpoint(api/me)

--- users/views.py
+++ users/views.py
@@ -3,6 +3,9 @@ from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import RegisterSerializer
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
@@ -17,3 +20,16 @@ class RegisterView(generics.CreateAPIView):
             'first_name': user.first_name,
             'last_name': user.last_name
         }, status=status.HTTP_201_CREATED)
+
+class Meview(APIView):
+    permission_classes = [IsAuthenticated] # only authenticated users may acces
+
+    def get(self, request):
+        user = request.user # DRF already automatically obtains the user from the JWT
+        return Response ({
+            'id': str(user.id),
+            'email': user.email,
+            'first_name': user.first_name,
+            'last_name': user.last_name
+        })
+
