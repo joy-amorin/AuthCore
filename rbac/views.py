470c0fd7dfa3b72201d92354af1ef7c13552c359
joy-amorin <amorinkaren2021@gmail.com>
2025-12-15 16:25:53 -0300

    fix(rbac): use RoleSerializer in RoleViewSet instead of RolePermissionSerializer

--- rbac/views.py
+++ rbac/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from .models import Permission, Role
-from .serializers import PermissionSerializer, RolePermissionSerializer
+from .serializers import PermissionSerializer, RoleSerializer
 
 # only admins can manage permissions
-class PermissionViewSet(viewsets.ModelViewSet):
+class PermissionViewSet(viewsets.ModelViewSet): # create automatically  GET, POST, PUT, DELETE with Model Viewset
     queryset = Permission.objects.all()
     serializer_class = PermissionSerializer
     permission_classes = [permissions.IsAdminUser]
 
 class RoleViewSet(viewsets.ModelViewSet):
     queryset = Role.objects.all()
-    serializer_class = RolePermissionSerializer
+    serializer_class = RoleSerializer
     permission_classes = [permissions.IsAdminUser]
     
