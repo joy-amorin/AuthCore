fecc91f046fae59a53516cc529667c04490f88e2
joy-amorin <amorinkaren2021@gmail.com>
2025-12-15 16:12:41 -0300

    add descriptive comments

--- rbac/urls.py
+++ rbac/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import RoleViewSet, PermissionViewSet
 
+# DefaultRouter: create automaticalli GET/api/roles, 
+# POST/api/roles, GET/api/roles<id>, PUT/DELETE
 router = DefaultRouter()
 router.register(r'roles', RoleViewSet)
 router.register(r'permissions', PermissionViewSet)
