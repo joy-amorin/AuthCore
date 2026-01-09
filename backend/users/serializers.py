25c5771793411f3a52fb823012569fba8ba1a783
joy-amorin <amorinkaren2021@gmail.com>
2026-01-09 15:18:38 +0000

    feat: expose permissions in api/me endpoint

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
+from rbac.models import Role, Permission
 
 User = get_user_model() # get custom user model
 
@@ -29,9 +30,10 @@ class UserSerializer(serializers.ModelSerializer):
 
 class MeSerializer(serializers.ModelSerializer):
     roles = serializers.SerializerMethodField()
+    permissions = serializers.SerializerMethodField()
     class Meta:
         model = User
-        fields = ['id', 'email', 'first_name', 'last_name', 'roles', 'is_superuser']
+        fields = ['id', 'email', 'first_name', 'last_name', 'roles', 'is_superuser', 'permissions']
         read_only_fields = fields
     
     def get_roles(self, obj):
@@ -40,3 +42,17 @@ class MeSerializer(serializers.ModelSerializer):
                 .select_related('role')
                 .values_list('role__name', flat=True)
         )
+    def get_permissions(self, obj):
+
+        if obj.is_superuser:
+            return list(
+                Permission.objects.values_list('name', flat=True)
+            )
+        roles = Role.objects.filter(role_assignments__user=obj
+                                    ).prefetch_related('permissions')
+        permission = set()
+        for role in roles:
+            for perm in role.permissions.all():
+                permission.add(perm.name)
+        return list(permission)
+        
